72a4413a59867a762022253a9d6ae2b1e03aed50
roland31x <roland31x@gmail.com>
2023-04-03 15:48:48 +0300

    Add project files.

---
+++ GC_C6_WPF/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GC_C6_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Point> points = new List<Point>();
+        
+        Line last;
+        Ellipse set;
+        bool done = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MouseDown += MainWindow_MouseDown;
+            MouseMove += MainWindow_MouseMove;
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(!done && points.Count > 0)
+            {
+                if(last != null)
+                {
+                    MainCanvas.Children.Remove(last);
+                }
+                double y = e.GetPosition(this).Y;
+                double x = e.GetPosition(this).X;
+                Point clicked = new Point(x, y);
+                HoverLine(points.Last(), clicked, Colors.Coral);                   
+            }
+        }
+
+        private void MainWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!done)
+            {
+                double y = e.GetPosition(this).Y;
+                double x = e.GetPosition(this).X;
+                Ellipse c = new Ellipse()
+                {
+                    Width = 5,
+                    Height = 5,
+                    Fill = new SolidColorBrush(Colors.Black),
+                    Stroke = new SolidColorBrush(Colors.Black),
+                };
+                if (points.Count == 0)
+                {
+                    Ellipse st = new Ellipse()
+                    {
+                        Width = 20,
+                        Height = 20,
+                        Fill = new SolidColorBrush(Colors.Orange),
+                        Stroke = new SolidColorBrush(Colors.Black),
+                    };
+                    st.MouseDown += Finish_Click;
+                    st.MouseEnter += St_MouseEnter;
+                    st.MouseLeave += St_MouseLeave;
+                    Canvas.SetTop(st, y - st.Width / 2);
+                    Canvas.SetLeft(st, x - st.Height / 2);
+                    MainCanvas.Children.Add(st);
+                    Canvas.SetZIndex(st, 1);
+                    set = st;
+                    c.MouseDown += Finish_Click;
+                }
+                
+                Canvas.SetTop(c, y - c.Width / 2);
+                Canvas.SetLeft(c, x - c.Height / 2);
+                MainCanvas.Children.Add(c);
+                Canvas.SetZIndex(c, 2);
+                Point clicked = new Point(x, y);
+                if (points.Count > 0)
+                {
+                    DrawLine(points.Last(), clicked, Colors.Gray);
+                }
+                points.Add(clicked);
+            }          
+        }
+
+        private void St_MouseLeave(object sender, MouseEventArgs e)
+        {
+            set.Fill = new SolidColorBrush(Colors.Orange);
+        }
+
+        private void St_MouseEnter(object sender, MouseEventArgs e)
+        {
+            set.Fill = new SolidColorBrush(Colors.Red);
+        }
+
+        private void Finish_Click(object sender, RoutedEventArgs e)
+        {
+            
+            DrawLine(points.First(), points.Last(), Colors.Gray);
+            done = true;
+            MainCanvas.Children.Remove(last);
+            MainCanvas.Children.Remove(set);
+        }
+        void DrawLine(Point p, Point q, Color c)
+        {
+            Line l = new Line()
+            {
+                X1 = p.X,
+                X2 = q.X,
+                Y1 = p.Y,
+                Y2 = q.Y,
+                StrokeThickness = 2,
+                Stroke = new SolidColorBrush(c),
+                Fill = new SolidColorBrush(c),
+            };
+            MainCanvas.Children.Add(l);
+        }
+        void HoverLine(Point p, Point q, Color c)
+        {
+            Line l = new Line()
+            {
+                X1 = p.X,
+                X2 = q.X - 1,
+                Y1 = p.Y,
+                Y2 = q.Y - 1,
+                StrokeThickness = 2,
+                Stroke = new SolidColorBrush(c),
+                Fill = new SolidColorBrush(c),
+            };
+            l.MouseDown += MainWindow_MouseDown;
+            MainCanvas.Children.Add(l);
+            Canvas.SetZIndex(l, -1);
+            last = l;
+        }
+
+        private void TR_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i < points.Count; i++)
+            {
+                for (int j = 1; j < points.Count; j++)
+                {
+                    if (!IsLeft(points[i], points[0], points[j]))
+                    {
+                        (points[i], points[j]) = (points[j], points[i]);
+                    }
+                }
+            }
+            for(int i = 2; i < points.Count - 1; i++)
+            {
+                DrawLine(points[0], points[i], Colors.Red);
+            }
+        }
+        bool IsLeft(Point r, Point p, Point q)
+        {
+            double Det = p.X * q.Y + p.Y * r.X + q.X * r.Y - r.X * q.Y - r.Y * p.X - q.X * p.Y;
+            if (Det < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            points = new List<Point>();
+            done = false;
+            MainCanvas.Children.Clear();
+        }
+    }
+}
